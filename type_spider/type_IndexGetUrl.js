54cd532fd11510abc80582fdff7c697a877be303
shadow <389570357@qq.com>
2016-09-21 21:27:18 +0800

    Update type_IndexGetUrl.js

--- type_spider/type_IndexGetUrl.js
+++ type_spider/type_IndexGetUrl.js
@@ -3,6 +3,7 @@ var cheerio=require('cheerio');//解析html
 var items=[];
 
 var count=900;//先了解共有几组数据
+var _id=[];
 
 fs.readFile('origin/ysindex.html',function(err,data){  
     if(err) { throw err;  } 
@@ -18,6 +19,8 @@ fs.readFile('origin/ysindex.html',function(err,data){
 		      var star=content('.star').attr("class").replace("star","").replace("star-","").replace(" ","");
 		      var price=content('.comment').text().replace("消费:","");
 		      var area=content('.intro').children().first().text();
+		      _id.push(url.replace("/shop/",""));
+
 		      items.push({
 		      	"name":name,
 		      	"id":url.replace("/shop/",""),
@@ -32,10 +35,16 @@ fs.readFile('origin/ysindex.html',function(err,data){
 		      count--;//递减
 		      if(count<=0){
 		      		//最后输出
+		      		console.log(count);
 		      		console.log("共"+items.length);
 		      		fs.writeFile("data/ys.json",JSON.stringify(items),function(err){
 					    if(!err)
 					    console.log("写入成功！")
+					});
+
+					fs.writeFile("data/ys_id.json",JSON.stringify(_id),function(err){
+					    if(!err)
+					    console.log(_id.length)
 					});
 
 		      };
@@ -45,6 +54,6 @@ fs.readFile('origin/ysindex.html',function(err,data){
 
 
     
-   console.log("00"+items.length); 
+  // console.log("00"+items.length); 
 
 });
