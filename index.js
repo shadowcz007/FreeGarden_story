1780bc3c22657e6ee15827d47cef7f9f921fdb8a
shadow <389570357@qq.com>
2016-10-07 14:10:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,34 +2,27 @@
 // verbose默认值为false,即不输出来自phantom的信息(请记住,casper是基于phantom的)  
 // logLevel表示何种级别输出信息,枚举为debug, info, warning, error  
 var fs=require('fs');
+ 
+
 var casper = require('casper').create({  
     verbose: true,  
     logLevel: 'erro'  
 });  
   
   
-
-  
-// 读取命令行的参数  
-var url = casper.cli.get("url"),  
-    fetch = casper.cli.get("fetch"),
-    category=casper.cli.get("category");  
-  
-  
- console.log(url);  
- console.log(fetch);  
- console.log(category);  
-  
   
 // casper.exit();  
   
-  
+ // 读取命令行的参数 
 var opts = {  
     url : casper.cli.get("url"),  
     fetch : casper.cli.get("fetch"),  
-    category : casper.cli.get("category")  
+    category : casper.cli.get("category"),
+    ifDownLoad : casper.cli.get("download")  
 };  
-  
+ 
+ console.log(opts);  
+
   
 // 用来计算时间  
 var startTime, endTime;  
@@ -50,19 +43,102 @@ casper.then(function(){startTime = + new Date;});
   
   
 // 使用fetch文件夹中相对应的方法去捕获信息  
+
 casper.then(function(){  
-    require("./fetch/"+fetch).call(casper,opts);  
-});  
+
+    if (opts.ifDownLoad) {
+
+        var json=fs.read('./data/'+opts.fetch.replace('.js','')+'/'+opts.category+'1.json');
+        
+        json=JSON.parse(json);
+        
+        opts.link=json;
+
+        this.echo('LoadJson---------------------------------------');
+        
+        require("./fetch/"+opts.fetch.replace(".js","Pic.js")).call(casper,opts);
+
+    }else{
+
+        require("./fetch/"+opts.fetch).call(casper,opts); 
+
+    };
+
+     
+}); 
+
+
+
+
+
+
+/*
+casper.then(function(){ 
+    var json=fs.read('./data/'+opts.fetch.replace('.js','')+'/'+opts.category+'1.json');
+    json=JSON.parse(json);
+    
+    opts.link=json;
+
+    this.echo('LoadJson');
+    
+    require("./fetch/"+opts.fetch.replace(".js","Pic.js")).call(casper,opts);
+}); 
+
+*/
+
 
 casper.on('saveToJson', function(data) {
-    //console.log(data);
+    console.log(data);
+ 
+
+    if (data.index==null) {
+        var json2=JSON.stringify(data,['url']);
+        //this.echo('okkkkk');
+        var json3=json2.replace(/\{"url":\[/g,'').replace(/\]\}/g,'');
+        /*
+        var linksArray=json3.split(",");
+        var ln=linksArray.length;
+
+        if (ln>1000) {
+          
+          var lns=Math.ceil(ln/1000);
+
+
+        };
+    */
+        fs.write('./data/'+opts.fetch.replace('.js','')+'/'+opts.category+'1.json',json3, 'w');
+        this.echo('piclinks,saveToJson1--------------'+opts.category);
+    };
+
+    if(data.index==0){
+        var json=JSON.stringify(data,undefined,2); 
+        fs.write('./data/'+opts.fetch.replace('.js','')+'/'+opts.category+data.index+'.json',json, 'w');
+        this.echo('urls,saveToJson0--------------'+opts.category);
+    };
+
+    if (data.index==2) {
+        var json=JSON.stringify(data,undefined,2); 
+        fs.write('./data/'+opts.fetch.replace('.js','')+'/'+opts.category+data.index+'.json',json, 'w');
+        this.echo('pic64basecode,saveToJson2--------------'+opts.category);
+    };
     
-    var json=JSON.stringify(data,undefined,2); 
-
-    fs.write('./data/'+fetch.replace('.js','')+'/'+category+data.index+'.json',json, 'w');
-
-    this.echo('saveToJson');
 });  
+
+//
+casper.on('LoadJson', function() {
+
+    var json=fs.read('./data/'+opts.fetch.replace('.js','')+'/'+opts.category+'0.json',json, 'w');
+
+    this.echo('LoadJson');
+    console.log(json);
+
+    casper.emit('downLoadPic',json);
+    
+}); 
+
+casper.on('downLoadPic',function(data){
+     require("./fetch/"+opts.fetch.replace(".js","")+"Pic.js").call(casper,[opts,data]);
+});
   
 // 记录结束时间  
 casper.then(function(){endTime = + new Date;});  
